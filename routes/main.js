a362e9ea206462adc914a9f0016c736327c66e05
Sriram <sriram01201@gmail.com>
2020-08-29 16:52:19 +0530

    Not finished

--- routes/main.js
+++ routes/main.js
@@ -76,10 +76,118 @@ app.get('/docs/:cname/:dbname',(req,res)=>{
                     properties=Object.getOwnPropertyNames(docs[i]);
                 }
             }
-            console.log(Object.getOwnPropertyNames(docs[0]));
-            res.render('docs.ejs',{c:await cdb.collection(cname).find().toArray(),dbname:dbname,properties:properties});
+            // console.log(Object.getOwnPropertyNames(docs[0]));
+            res.render('docs.ejs',{c:await cdb.collection(cname).find().toArray(),dbname:dbname,properties:properties,dbname:dbname,cname:cname});
     });    
     // console.log(dbs);
 });
-
+app.post('/dbdelete/:dbname',(req,res)=>{
+    var dbname=req.params.dbname;
+    mongodb.MongoClient.connect('mongodb://localhost/', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },async (err,db)=>{
+        var cdb=db.db(dbname);
+        await cdb.dropDatabase();
+        res.redirect('/databases');
+    });
+});
+app.post('/cdelete/:dbname/:cname',(req,res)=>{
+    var dbname=req.params.dbname;
+    var cname=req.params.cname;
+    mongodb.MongoClient.connect('mongodb://localhost/', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },async (err,db)=>{
+        var cdb=db.db(dbname);
+        await cdb.dropCollection(cname);
+        res.redirect('/collections/'+dbname);
+    });
+});
+app.post('/adddb',(req,res)=>{
+    var dbname=req.body.dbname;
+    var cname=req.body.cname;
+    var already=0;
+    mongodb.MongoClient.connect('mongodb://localhost/', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },async (err,db)=>{
+            if(err){
+                console.log(err);
+            }
+            var admindb= db.db().admin();
+            var dbs =await admindb.listDatabases();
+            for(var i=0;i<dbs.databases.length;t++){
+                if(dbs[i].databases.name==dbname){
+                    already=1;
+                }
+            }
+    });
+    if(already==0){
+        mongodb.MongoClient.connect('mongodb://localhost/'+dbname, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },async (err,db)=>{
+            var cdb=db.db(dbname);
+            await cdb.createCollection(cname);
+            res.redirect('/databases');
+        });
+    }
+    res.redirect('/databases');
+});
+app.post('/addelement/:dbname/:cname',(req,res)=>{
+    var cname=req.params.cname;
+    var dbname=req.params.dbname;
+    var jsontxt=req.body.jsontxt;
+    var newobj=JSON.parse(jsontxt);
+    mongodb.MongoClient.connect('mongodb://localhost/', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },async (err,db)=>{
+        var cdb=db.db(dbname);
+        await cdb.collection(cname).insertOne(newobj);
+        res.redirect('/docs/'+cname+'/'+dbname);
+    });
+});
+app.post('/upelement/:dbname/:cname',(req,res)=>{
+    var cname=req.params.cname;
+    var dbname=req.params.dbname;
+    var jsontxt=req.body.jsontxt;
+    var setobj=JSON.parse(jsontxt);
+    var newobj=setobj;
+    delete setobj._id;
+    console.log(Object.getOwnPropertyNames(setobj)[0]);
+    var setobjstr='{ $set:';
+    // for(var i=0;i<Object.getOwnPropertyNames(setobj).length;i++){
+    //     setobjstr+= Object.getOwnPropertyNames(setobj)[i]
+    //     setobjstr+= ':'
+    //     setobjstr+= setobj[Object.getOwnPropertyNames(setobj)[i]];
+    // }
+    var id=(newobj._id);
+    setobjstr+=JSON.stringify(setobj)
+    setobjstr+='}';
+    console.log(setobjstr);
+    mongodb.MongoClient.connect('mongodb://localhost/', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },async (err,db)=>{
+        var cdb=db.db(dbname);
+        await cdb.collection(cname).updateOne({_id: new mongodb.ObjectID(id)},{$set: setobj},{ upsert: false });
+        res.redirect('/docs/'+cname+'/'+dbname);
+    });
+});
+app.post('/elementdelete/:dbname/:cname/:id',(req,res)=>{
+    var cname=req.params.cname;
+    var dbname=req.params.dbname;
+    var id=req.params.id;
+    console.log(id);
+    mongodb.MongoClient.connect('mongodb://localhost/', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },async (err,db)=>{
+        var cdb=db.db(dbname);
+        await cdb.collection(cname).deleteOne({_id: new mongodb.ObjectID(id)});
+        res.redirect('/docs/'+cname+'/'+dbname);
+    });
+});
 module.exports =app;
